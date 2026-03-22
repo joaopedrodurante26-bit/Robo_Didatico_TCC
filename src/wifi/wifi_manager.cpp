b6b6744040f0a8ec1ca069ecefbcfae7b0212ac6
joaopedrodurante26-bit <joaopedrodurante26@gmail.com>
2026-03-22 20:01:35 -0300

    criando o servidor e a pagina web

--- src/wifi/wifi_manager.cpp
+++ src/wifi/wifi_manager.cpp
@@ -1,11 +1,48 @@
 #include "wifi_manager.h"
+
+#include <Arduino.h>
 #include <WiFi.h>
+#include <WebServer.h>
+#include <LittleFS.h>
 
 // =========================
 // CONFIGURAÇÕES DA REDE
 // =========================
 const char* ssid = "ROBO_VESPA";
 const char* password = "12345678"; // mínimo 8 caracteres
+
+// =========================
+// SERVIDOR WEB
+// =========================
+WebServer server(80);
+
+// =========================
+// CONFIGURA ROTAS
+// =========================
+void configurarRotas() {
+
+    server.on("/", []() {
+        File file = LittleFS.open("/index.html", "r");
+
+        if (!file) {
+            server.send(500, "text/plain", "Erro ao abrir HTML");
+            return;
+        }
+
+        server.streamFile(file, "text/html");
+        file.close();
+    });
+
+    server.on("/frente", []() {
+        Serial.println("[CMD] Frente");
+        server.send(200, "text/plain", "OK");
+    });
+
+    server.on("/parar", []() {
+        Serial.println("[CMD] Parar");
+        server.send(200, "text/plain", "OK");
+    });
+}
 
 // =========================
 // INICIALIZAÇÃO DO WIFI
@@ -23,13 +60,24 @@ void initWiFi() {
     Serial.print(ssid);
     Serial.print("\n[WIFI] IP: ");
     Serial.println(IP);
+
+    // Inicializa sistema de arquivos
+    if (!LittleFS.begin()) {
+        Serial.println("[ERRO] Falha ao montar LittleFS");
+        return;
+    }
+
+    Serial.println("[FS] Sistema de arquivos montado");
+
+    configurarRotas();
+    server.begin();
+
+    Serial.println("[SERVER] Servidor iniciado");
 }
 
 // =========================
 // LOOP WIFI (futuro)
 // =========================
 void atualizarWiFi() {
-    // Aqui vamos futuramente:
-    // - tratar requisições web
-    // - atualizar dados
+    server.handleClient();
 }
