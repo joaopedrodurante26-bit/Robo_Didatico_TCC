d655469e78d789922f55f1d46bbdd2c0610c75a9
joaopedrodurante26-bit <joaopedrodurante26@gmail.com>
2026-03-22 19:25:57 -0300

    criação da rede wifi do robô para comunicação e controle

--- src/wifi/wifi_manager.cpp
+++ src/wifi/wifi_manager.cpp
@@ -1,9 +1,35 @@
 #include "wifi_manager.h"
+#include <WiFi.h>
 
+// =========================
+// CONFIGURAÇÕES DA REDE
+// =========================
+const char* ssid = "ROBO_VESPA";
+const char* password = "12345678"; // mínimo 8 caracteres
+
+// =========================
+// INICIALIZAÇÃO DO WIFI
+// =========================
 void initWiFi() {
-    // Configuração futura do servidor web
+    Serial.println("[WIFI] Iniciando modo Access Point...");
+
+    // Inicia como ponto de acesso
+    WiFi.softAP(ssid, password);
+
+    IPAddress IP = WiFi.softAPIP();
+
+    Serial.print("[WIFI] Rede criada!");
+    Serial.print("\n[WIFI] SSID: ");
+    Serial.print(ssid);
+    Serial.print("\n[WIFI] IP: ");
+    Serial.println(IP);
 }
 
+// =========================
+// LOOP WIFI (futuro)
+// =========================
 void atualizarWiFi() {
-    // Comunicação futura
+    // Aqui vamos futuramente:
+    // - tratar requisições web
+    // - atualizar dados
 }
