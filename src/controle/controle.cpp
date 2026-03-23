cd2b106df11ff1dac64b1070a3625e1d3d5df866
joaopedrodurante26-bit <joaopedrodurante26@gmail.com>
2026-03-23 14:12:07 -0300

    comecei a estrutura dos modulos de controle

--- src/controle/controle.cpp
+++ src/controle/controle.cpp
@@ -1,9 +1,24 @@
 #include "controle.h"
 
+String comandoAtual = "parar";
+
 void initControle() {
-    // Inicialização da lógica
+    comandoAtual = "parar";
+}
+
+void setComando(const String& cmd) {
+    comandoAtual = cmd;
 }
 
 void atualizarControle() {
-    // Tomada de decisão
-}
+    if (comandoAtual == "frente") {
+    // mover frente
+    } else if (comandoAtual == "tras") {
+        // mover tras
+    } else if (comandoAtual == "esquerda") {
+        // mover esquerda
+    } else if (comandoAtual == "direita") {
+        // mover direita
+    } else if (comandoAtual == "parar") {
+        // parar movimento
+    }
--- src/controle/controle.h
+++ src/controle/controle.h
@@ -1,7 +1,9 @@
 #ifndef CONTROLE_H
 #define CONTROLE_H
+#include <Arduino.h>
 
 void initControle();
+void setComando(const String& cmd);
 void atualizarControle();
 
 #endif
