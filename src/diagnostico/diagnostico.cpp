d4c971b23823aebc21452a01e889119c8641b826
joaopedrodurante26-bit <joaopedrodurante26@gmail.com>
2026-04-05 13:40:54 -0300

    criei o modulo diagnostico.cpp

---
+++ src/diagnostico/diagnostico.cpp
@@ -0,0 +1,39 @@
+#include "diagnostico.h"
+#include "../sensores/sensores.h"
+#include <LittleFS.h>
+
+void executarDiagnostico() {
+
+    String log = "{";
+
+    // =========================
+    // MPU6050
+    // =========================
+    bool imu_ok = (getAccelX() != 0 || getAccelY() != 0 || getAccelZ() != 0);
+    log += "\"imu\": " + String(imu_ok ? "true" : "false") + ",";
+
+    // =========================
+    // Ultrassônico
+    // =========================
+    float dist = getDistancia();
+    bool ultra_ok = (dist > 0);
+    log += "\"ultrassonico\": " + String(ultra_ok ? "true" : "false") + ",";
+
+    // =========================
+    // Encoders
+    // =========================
+    bool enc_ok = true; // (pode melhorar depois)
+    log += "\"encoders\": " + String(enc_ok ? "true" : "false");
+
+    log += "}";
+
+    // =========================
+    // Salvar arquivo
+    // =========================
+    File file = LittleFS.open("/boot_log.json", "w");
+
+    if (file) {
+        file.print(log);
+        file.close();
+    }
+}
